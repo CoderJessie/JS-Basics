fcc329a78fdbfc45275fbaadef62dc6a0a4f67db
Ogbondah Jessica <ogbondahj@gmail.com>
2022-08-22 12:55:15 +0100

    code challenge #4

--- script.js
+++ script.js
@@ -448,7 +448,6 @@ if (day === 'monday') {
 } else {
 	console.log('Not a valid day!');
 }
-*/
 
 //STATEMENT AND EXPRESSIONS
 //Expression is a piece of code that produces a value.
@@ -465,3 +464,45 @@ if (23 > 10) {
 //Example in template literal:
 const me = 'Jonas';
 console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+//The Conditional Statement
+//It allows us to write something similar to the if/else statement but all in one line.
+const age = 23;
+// age >= 18
+// 	? console.log('I like to drink wine ')
+// 	: console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+	drink2 = 'wine';
+} else {
+	drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+*/
+////////////////////////////////////////
+////Coding Challenge #4
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement (If it's easier for you, you can start with an if/else statment, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430
+
+HINT: To calculate 20% of a value, simply multiply by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300
+
+GOOD LUCK
+*/
+
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+	`The was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`
+);
