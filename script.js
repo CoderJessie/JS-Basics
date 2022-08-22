7645e77f9d11f901205914a5c489a1af25cf48e7
Ogbondah Jessica <ogbondahj@gmail.com>
2022-08-22 12:57:32 +0100

    updated code challenge #4

--- script.js
+++ script.js
@@ -501,7 +501,7 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300
 GOOD LUCK
 */
 
-const bill = 430;
+const bill = 275;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 console.log(
 	`The was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`
